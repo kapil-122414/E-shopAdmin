50eae0fd1d03c2f1fddd326d81db42339c86e593
kapil-122414 <kapilkumar122414@gmail.com>
2026-08-31 14:52:08 +0530

    add the same error

--- E-commerce-adminpanel/E-shop-Admin/src/features/brands/api.js
+++ E-commerce-adminpanel/E-shop-Admin/src/features/brands/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: "https://categoryapi-oluc.onrender.com/api",
+  baseURL: "/api",
   withCredentials: true,
 });
 export const brandpost = (data) =>
